0534a923b494e808a288800bb7df4e9d3f1a8108
Ronald Moran Jr <ronaldmoran930@gmail.com>
2025-08-31 00:25:10 +0800

    Add PlayVideo component with video player, publisher info, actions and sample comments

--- youtube-clone/src/Pages/Video/Video.jsx
+++ youtube-clone/src/Pages/Video/Video.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Video.css"; // Assuming you have a CSS file for styling
+import PlayVideo from "../../Components/PlayVideo/PlayVideo";
 
 const Video = () => {
   return (
     <div className="play-container">
-      
+        <PlayVideo />
     </div>
   );
 }
